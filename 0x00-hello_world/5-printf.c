e6e203c29958ad06173737cfc557b925df3f49fe
successmarcus31 <marcus.success@gmail.com>
2022-09-08 09:01:27 -0700

    Task 6 and 7

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence with the printf function
+ *
+ * Return: returns 0 after printing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
