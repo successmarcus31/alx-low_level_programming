e485963fed1528f60d46fef9a2b4dee07210b53a
Success-Marcus <marcus.success@gmail.com>
2022-09-09 12:27:07 +0100

    Task: 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - where programs are executed
+ *
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar (alpha);
+	}
+	putchar ('\n');
+	return (0);
+}
